b3a294d7cf201d4979d9c5bee8175228fe4c3c3e
italofgcoura <33141562+italofgcoura@users.noreply.github.com>
2019-10-12 12:58:57 -0300

    Menu
    Utilizando angular materials

--- arca/src/app/app.module.ts
+++ arca/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
