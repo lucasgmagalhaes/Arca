c5f258c24255ec9870d0cb804cfc5c80bfbeb32f
italofgcoura <33141562+italofgcoura@users.noreply.github.com>
2019-10-12 18:12:41 -0300

    Menu
    Criação do menu

--- arca/src/app/app.module.ts
+++ arca/src/app/app.module.ts
@@ -3,19 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MenuComponent } from './menu/menu.component';
-import { MatMenuModule } from '@angular/material/menu';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatMenuModule
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
