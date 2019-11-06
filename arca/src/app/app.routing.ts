b5d2b87e370a57c2c4406f0b6b873f73905dd5f6
italofgcoura <italofabricio@outlook.pt>
2019-11-06 16:21:45 -0300

    Routing cadastro animais

--- arca/src/app/app.routing.ts
+++ arca/src/app/app.routing.ts
@@ -11,14 +11,13 @@ import { LoginComponent } from './login/login.component';
 import { CadastroFuncionarioComponent } from './cadastro-funcionario/cadastro-funcionario.component';
 import { CadastroParceiroComponent } from './cadastro-parceiro/cadastro-parceiro.component';
 
-
 export const ROUTES: Routes =
   [{ path: 'cadastro-animal', component: CadastroAnimalComponent },
+  { path: 'cadastro-funcionario', component: CadastroFuncionarioComponent },
+  { path: 'cadastro-parceiro', component: CadastroParceiroComponent },
   { path: 'doacao', component: DoacaoComponent },
   { path: 'servicos', component: HomeServicosComponent },
   { path: 'adocao', component: HomeAdocaoComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'cadastro-funcionario', component: CadastroFuncionarioComponent },
-  { path: 'cadastro-parceiro', component: CadastroParceiroComponent },
-  { path: '', pathMatch: 'full', redirectTo:'/home' }];
+  { path: '', pathMatch: 'full', redirectTo: '/home' }];
